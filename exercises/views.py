65a17a4c33381cdeab110c6b636a79e8b9882a76
CallaJun <maracondo@aol.com>
2015-01-17 09:30:43 -0500

    More pagination

--- exercises/views.py
+++ exercises/views.py
@@ -8,13 +8,10 @@ def home(request):
 		locals(), 
 		context_instance=RequestContext(request))
 
-
 # go through daily exercises
 
 
 def dailyExercises(request):
-	"""Goes through array of set exercises, inputing user data
-	for each."""
 	exercise_list = ['e1', 'e2', 'e3']
 	#put one element of data onto each page
 	paginator = Paginator(exercise_list, 1)
@@ -30,5 +27,6 @@ def dailyExercises(request):
 		#if page is out of range (e.g. 9999), deliver last page of results.
 		exercises = paginator.page(paginator.num_pages)
 
-	return render_to_response('list.html', {"exercises": exercises})
+	return render_to_response('list.html', {"exercises": exercises,
+											"num_pages": exercise_list})
 
